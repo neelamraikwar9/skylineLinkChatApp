a2014cc5aee9a06fce97fdf3d23b6de85039c9a1
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-02-24 00:56:14 +0530

    chore: modified file

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -5,11 +5,12 @@ const Home = () => {
   return (
     <Container maxW='xl' centerContent>
     <Box d="flex" justifyContent="center" padding={3} bg={"white"} w="100%" m="40px 0 15px 0" borderRadius="1g" borderWidth="1px">
-      <Text fontSize="4xl">Skyline Link</Text>
+      <Text fontSize="4xl" fontFamily={'cursive'} color="black">Skyline Link</Text>
     </Box>
       
-      <Box>
-
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+      
+      
       </Box>
     </Container>
   );
